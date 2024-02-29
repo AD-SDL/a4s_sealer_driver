a4eda3863fef1ed427c7700b04cb41869dde65bb
Dozgulbas <dozgulbas@anl.gov>
2024-02-29 15:45:50 -0600

    fix

--- scripts/a4s_sealer_rest_node.py
+++ scripts/a4s_sealer_rest_node.py
@@ -1,4 +1,5 @@
 """The server that takes incoming WEI flow requests from the experiment application"""
+
 import json
 from argparse import ArgumentParser
 from contextlib import asynccontextmanager
@@ -20,8 +21,27 @@ from a4s_sealer_driver.a4s_sealer_driver import (
     A4S_SEALER_DRIVER,
 )
 
-global sealer, state
-device = ""
+
+parser = ArgumentParser()
+parser.add_argument(
+    "--host",
+    type=str,
+    default="0.0.0.0",
+    help="Hostname that the REST API will be accessible on",
+)
+parser.add_argument("--port", type=int, default=3001)
+parser.add_argument(
+    "--device",
+    type=str,
+    default="/dev/ttyUSB2",
+    help="Serial device for communicating with the device",
+)
+args = parser.parse_args()
+
+global sealer, state, device
+
+device = args.device
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -58,7 +78,7 @@ app = FastAPI(
 def get_state():
     global sealer, state
     if state != "BUSY":
-        sealer.get_status() 
+        sealer.get_status()
         if sealer.status_msg == 3:
             # msg.data = "State: ERROR"
             state = "ERROR"
@@ -88,13 +108,13 @@ async def about() -> JSONResponse:
                         description="The amount of time for sealing a plate.",
                         type="int",
                         required=True,
-                    ), 
+                    ),
                     ModuleActionArg(
                         name="temperature",
-                        description="The temperature to heat the plate to when sealing it.", 
-                        type="int", 
-                        required=True
-                    )
+                        description="The temperature to heat the plate to when sealing it.",
+                        type="int",
+                        required=True,
+                    ),
                 ],
             )
         ],
@@ -114,14 +134,12 @@ def do_action(
     action_handle: str,
     action_vars: str,
 ):
-
     global sealer, state
     state = "BUSY"
     if action_handle == "seal":
         # self.sealer.set_time(3)
         # self.sealer.set_temp(175)
         try:
-
             sealer.seal()
             time.sleep(15)
             response_content = {
@@ -143,16 +161,10 @@ def do_action(
 
 if __name__ == "__main__":
     import uvicorn
-    parser = ArgumentParser()
-    parser.add_argument("--host", type=str, default="0.0.0.0", help="Hostname that the REST API will be accessible on")
-    parser.add_argument("--port", type=int, default=3001)
-    parser.add_argument("--device", type=str, default="/dev/ttyUSB1", help="Serial device for communicating with the device")
-    args = parser.parse_args()
-    device = args.device
 
     uvicorn.run(
         "a4s_sealer_rest_node:app",
         host=args.host,
         port=args.port,
-        reload=False,
+        reload=True,
     )
