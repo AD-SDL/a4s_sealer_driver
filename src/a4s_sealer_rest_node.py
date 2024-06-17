87456b88b465c6314e41f737014ec9633140070c
Dozgulbas <dozgulbas@anl.gov>
2024-06-17 15:13:30 -0700

    Fixing module names

--- src/a4s_sealer_rest_node.py
+++ src/a4s_sealer_rest_node.py
@@ -1,19 +1,17 @@
 """REST-based node for A4S Sealer device"""
 
-import traceback
 from pathlib import Path
 
 from a4s_sealer_driver import A4S_SEALER_DRIVER
 from fastapi.datastructures import State
 from wei.modules.rest_module import RESTModule
-from wei.types.module_types import ModuleStatus
 from wei.utils import extract_version
 
 rest_module = RESTModule(
-    name="ur_node",
+    name="sealer_node",
     version=extract_version(Path(__file__).parent.parent / "pyproject.toml"),
-    description="A node to control the ur plate moving robot",
-    model="ur",
+    description="A node to control the A4S Sealer device",
+    model="A4S Sealer",
 )
 rest_module.arg_parser.add_argument(
     "--device",
@@ -24,15 +22,11 @@ rest_module.arg_parser.add_argument(
 
 
 @rest_module.startup()
-def ur_startup(state: State):
-    """UR startup handler."""
-    try:
-        state.sealer = None
-        state.sealer = A4S_SEALER_DRIVER(state.device)
-        state.status = ModuleStatus.IDLE
-    except Exception:
-        state.status = ModuleStatus.ERROR
-        traceback.print_exc()
-        print("CONNECTION FAILED")
-    else:
-        print("Sealer online")
+def sealer(state: State):
+    """Sealer startup handler."""
+    state.sealer = A4S_SEALER_DRIVER(state.device)
+    print("Sealer online")
+
+
+if __name__ == "__main__":
+    rest_module.start()
