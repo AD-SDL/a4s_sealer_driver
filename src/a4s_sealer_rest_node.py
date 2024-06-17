e7fbf66bcafb3c74492bca51912e21da4bf33c9f
Dozgulbas <dozgulbas@anl.gov>
2024-06-17 15:20:43 -0700

    Adding state handler

--- src/a4s_sealer_rest_node.py
+++ src/a4s_sealer_rest_node.py
@@ -1,11 +1,11 @@
 """REST-based node for A4S Sealer device"""
-
+import datetime
 from pathlib import Path
 
 from a4s_sealer_driver import A4S_SEALER_DRIVER
 from fastapi.datastructures import State
 from wei.modules.rest_module import RESTModule
-from wei.utils import extract_version
+from wei.utils import ModuleState, ModuleStatus, extract_version
 
 rest_module = RESTModule(
     name="sealer_node",
@@ -26,6 +26,27 @@ def sealer(state: State):
     """Sealer startup handler."""
     state.sealer = A4S_SEALER_DRIVER(state.device)
     print("Sealer online")
+
+
+@rest_module.state_handler()
+def state(state: State):
+    """Returns the current state of the UR module"""
+    if state.status not in [
+        ModuleStatus.BUSY,
+        ModuleStatus.ERROR,
+        ModuleStatus.INIT,
+        None,
+    ] or (
+        state.action_start
+        and (datetime.datetime.now() - state.action_start > datetime.timedelta(0, 2))
+    ):
+        state.sealer.get_status()
+        if state.sealer.status_msg == 3:
+            state.status = ModuleStatus.ERROR
+        elif state.sealer.status_msg == 0:
+            state.status = ModuleStatus.IDLE
+
+    return ModuleState(status=state.status, error="")
 
 
 if __name__ == "__main__":
